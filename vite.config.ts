816366ff97438679587f7b774fa769c5b1ffe5ae
unknown <noodle.hell@gmail.com>
2025-04-19 12:17:02 +0300

    fix paths for build

--- vite.config.ts
+++ vite.config.ts
@@ -41,14 +41,14 @@ export default defineConfig({
         entryFileNames: 'assets/js/[name]-[hash].js',
         assetFileNames: ({ name }) => {
           if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
-            return 'assets/images/[name]-[hash][extname]';
+            return './assets/images/[name]-[hash][extname]';
           }
           
           if (/\.css$/.test(name ?? '')) {
-            return 'assets/css/[name]-[hash][extname]';
+            return './assets/css/[name]-[hash][extname]';
           }
 
-          return 'assets/[name]-[hash][extname]';
+          return './assets/[name]-[hash][extname]';
         }
       }
     },
